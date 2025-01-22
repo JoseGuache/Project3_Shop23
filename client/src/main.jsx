a0ebaeb1c26c6363c18867a67f0ca61b748e3195
Mattvogelsang <vogelsangmatt@gmail.com>
2025-01-21 20:16:40 -0500

    saved

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import App from './App.jsx';
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+import NoMatch from './pages/NoMatch';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Success from './pages/Success';
+import OrderHistory from './pages/OrderHistory';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    error: <NoMatch />,
+    children: [
+      {
+        index: true, 
+        element: <Home />
+      }, {
+        path: '/login',
+        element: <Login />
+      }, {
+        path: '/signup',
+        element: <Signup />
+      }, {
+        path: '/success',
+        element: <Success />
+      }, {
+        path: '/orderHistory',
+        element: <OrderHistory />
+      }, {
+        path: '/products/:id',
+        element: <Detail />
+      }
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
 )
