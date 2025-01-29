89ee264203a1064c8adf255ec1acedf7e6a424a5
James Guache <jamesg2126@gmail.com>
2025-01-28 19:58:56 -0500

    Added pictures, descriptions, and prices too all kids shoes

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -231,111 +231,111 @@ db.once('open', async () => {
       quantity: 600
     },
     {
-      name: 'name',
+      name: 'Air Jordan 1 Retro "Pale Pink" (GS)',
       category: categories[2]._id,
       description:
-        'description',
-      image: 'AirJordan1-SG.jpg',
-      price: 699.99,
+        'Pick pink for the win! This pair pops to life with a bright mix of rosy hues. All your classic Jordan comforts are there too: Nike Air in the sole for a cushioned step, and that rubber cupsole for everyday comfort. Leather offers durability and structure. Encapsulated Nike Air-Sole units provide lightweight cushioning. Solid rubber outsoles give you everyday traction. Wings logo stamped on collar. Stitched-down Swoosh logo. Jumpman Air design on tongue. Textile lining and insole.',
+      image: 'AirJordan1PalePinkMid.jpg',
+      price: 269.99,
       quantity: 600
     },
     {
-      name: 'name',
+      name: 'Air Jordan 4 Retro "Cool Gray" (GS)',
       category: categories[2]._id,
       description:
-        'description',
-      image: 'AirJordan1-SG.jpg',
-      price: 699.99,
+        'After originally releasing in 2004, the Cool Grey 4 has not since been retroed until 2019. The "Cool Grey" color garnered attention when it was featured on a pair of Air Jordan 11 Retros in 2001. It seemed only fitting for Jordan to carry this tremendous colorway to a very notable design with the Air Jordan 4.',
+      image: 'AirJordan4KidsCoolGray.jpg',
+      price: 299.99,
       quantity: 600
     },
     {
-      name: 'name',
+      name: 'Air Jordan 4 Retro "Metallic Green" (GS)',
       category: categories[2]._id,
       description:
-        'description',
-      image: 'AirJordan1-SG.jpg',
-      price: 699.99,
+        'The Jordan 4 Retro Metallic Green (GS) is a striking colorway of the iconic Air Jordan 4 silhouette. This shoe features a combination of white and Metallic Green accents, creating a unique and eye-catching design.',
+      image: 'AirJordan4MetallicGreen.jpg',
+      price: 399.99,
       quantity: 600
     },
     {
-      name: 'name',
+      name: 'Air Jordan 4 Retro "Winterized Loyal Blue" (GS)',
       category: categories[2]._id,
       description:
-        'description',
-      image: 'AirJordan1-SG.jpg',
-      price: 699.99,
+        'The Air Jordan 4 Retro Winterized GS features a Loyal Blue leather upper and a rubberized synthetic material on the throat and mid-panel netting. The tongue utilizes the same rubberized material while the inner lining sports a fleece-like fabric for added warmth and comfort. A tonal heel plate with black Jumpman branding, a black mud guard, and a visible Air unit in the heel round out the look of the durable, redesigned Jordan 4.',
+      image: 'AirJordan4RetroWinterized.jpg',
+      price: 299.99,
       quantity: 600
     },
     {
-      name: 'name',
+      name: 'Air Jordan 4 Retro "White Cement" (GS)',
       category: categories[2]._id,
       description:
-        'description',
-      image: 'AirJordan1-SG.jpg',
-      price: 699.99,
+        'This sneaker features a white leather upper with speckled Cement Grey accents on the heel tabs and support wings. Hits of Fire Red on the tongue branding and black on the eyelets and heel branding contrast the upper. Other details include mesh netting on the sides and tongue, white laces, and black tongue lining. A black, Tech Grey, and Fire Red sole unit with speckled Cement Grey accents on the midsole and a visible Air window at the heel rounds out this sneaker’s design.',
+      image: 'AirJordan4WhiteCement.jpg',
+      price: 399.99,
       quantity: 600
     },
     {
-      name: 'name',
+      name: 'Jordan 11 Retro "Heiress Black Stingray" (GS)',
       category: categories[2]._id,
       description:
-        'description',
-      image: 'AirJordan1-SG.jpg',
-      price: 699.99,
+        'The Air Jordan 11 Retro Heiress Black Stingray GS comes in a black, metallic gold and white infrared color scheme. This sneaker boasts a glittering metallic top made of black suede. The mudguard on this Retro Heiress Air Jordan 11 in black for grade schoolers features an overall shiny appearance. Additionally, the player number of Michael Jordan, 23, is embroidered in a gold hue at the heel of this shoe.',
+      image: 'AirJordan11RetroBlackStingray.jpg',
+      price: 399.99,
       quantity: 600
     },
     {
-      name: 'name',
+      name: 'Jordan 11 Retro Low "Cool Grey" (GS)',
       category: categories[2]._id,
       description:
-        'description',
-      image: 'AirJordan1-SG.jpg',
-      price: 699.99,
+        'The Air Jordan 11 Retro Low Cool Grey GS, which is constructed in low-top design, has a a darker grey plain leather overlay, and a cool grey nubuck upper.',
+      image: 'AirJordan11RetroCoolGreyLow.jpg',
+      price: 274.99,
       quantity: 600
     },
     {
-      name: 'name',
+      name: 'Jordan 4 Retro "University Blue" (GS)',
       category: categories[2]._id,
       description:
-        'description',
-      image: 'AirJordan1-SG.jpg',
-      price: 699.99,
+        'Released in 2021, the Air Jordan 4 Retro University Blue GS brings a silhouette with a suede upper and a popular shade of blue inspired by the University of North Carolina. It has a bright blue pallet often with a Jumpman icon on the back, tongue, and sole. The upper made from University Blue Suede and black plastic lace holders on its throat.',
+      image: 'AirJordanRetro4KidsBlueUni.jpg',
+      price: 249.99,
       quantity: 600
     },
     {
-      name: 'name',
+      name: 'Jordan 3 retro "Black Cement" (GS)',
       category: categories[2]._id,
       description:
-        'description',
-      image: 'AirJordan1-SG.jpg',
-      price: 699.99,
+        'Originally released in 1988 and last seen in 2013, the Black Cement (2024) (GS) Jordan 3 Retro boasts a tumbled leather upper in black with tonal stitching. Elephant print decorates the leather overlays at the forefoot and heel, while perforations add texture to the gray leather around the collar. Pops of Fire Red stand out on the Jumpman logo, the first three eyelets, the bottom of the shoe, and the underside of the tongue. Padding in the tongue and collar completes the look and protects the ankle.',
+      image: 'Jordan-3-Black-Cement-Kids.jpg',
+      price: 99.99,
       quantity: 600
     },
     {
-      name: 'name',
+      name: 'Jordan 4 Retro "Lightning" (GS)',
       category: categories[2]._id,
       description:
-        'description',
-      image: 'AirJordan1-SG.jpg',
-      price: 699.99,
+        'Similar to the original Air Jordan 4 Retro Lightning, the 2021 Grade School edition features a mid-top with a bright yellow nubuck upper. The yellow nubuck upper boasts black molded eyelets and matching black netting. In addition to its impressive mid-top, the Air Jordan 4 Retro Lightning 2021 GS also features a molded Jumpman logo as well as dark grey accents on its wings, a white foam midsole, and a grey rubber outsole.',
+      image: 'Jordan4RetroLightning.jpg',
+      price: 174.99,
       quantity: 600
     },
     {
-      name: 'name',
+      name: 'Jordan 5 Retro "Racer Blue" (GS)',
       category: categories[2]._id,
       description:
-        'description',
-      image: 'AirJordan1-SG.jpg',
-      price: 699.99,
+        'The grade school sized (GS) Air Jordan 5 Retro Racer Blue GS uses a Black/Racer Blue/Reflective Silver colorway on this vintage 1990 silhouette. A bright blue midsole and a silver tongue pop against a black-on-black upper made of suede and nubuck. A blue Jumpman appears on the black heel, the silver tongue, and the white part of the outsole. The Racer Blue midsole features a jagged black hit and a visible Air unit. The outsole is matte black and translucent white with a "JORDAN" insignia.',
+      image: 'Jordan5RetroMidRacerBlue.jpg',
+      price: 199.99,
       quantity: 600
     },
     {
-      name: 'name',
+      name: 'Jordan 13 Retro "Black University Blue" (GS)',
       category: categories[2]._id,
       description:
-        'description',
-      image: 'AirJordan1-SG.jpg',
-      price: 699.99,
+        'The upper of the Air Jordan 13 Retro Black University Blue (GS) is made from a mix of textured leather and mesh materials. The upper also features a 3M reflective design pattern that gives an eye-catching effect when it reflects light. The shoe features a Phylon midsole with Zoom Air technology in the heel and forefoot, which offers excellent cushioning and responsiveness. The shoe also has a padded tongue and collar that provides added comfort and support for the wearer. Inside the shoe, there is a mesh lining that helps to wick away moisture, keeping the feet dry and comfortable during extended use.',
+      image: 'Jordan13RetroBlackLTBlue.jpg',
+      price: 159.99,
       quantity: 600
     },
   ]);
