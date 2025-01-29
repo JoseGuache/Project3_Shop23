d341a9a31fa82798bcb072e470d307ef633ed19a
Mattvogelsang <vogelsangmatt@gmail.com>
2025-01-28 20:47:11 -0500

    fix manu bar and css with it

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -17,45 +17,77 @@ function DropdownItemTagsExample({ isLoggedIn }) {
   };
 
   return (
-    <DropdownButton
-      id="dropdown-item-button"
-      title="Menu"
-      show={isDropdownOpen}
-      onClick={handleToggle}
-      className="orange-button"
-    >
-      <Dropdown.Item className="dropdown-item-spacing" as={Link} to="/" onClick={handleItemClick}>
-        Home
-      </Dropdown.Item>
-      {/* <Dropdown.Item className="dropdown-item-spacing" as={Link} to="/category" onClick={handleItemClick}>
-        Men
-      </Dropdown.Item>
-      <Dropdown.Item className="dropdown-item-spacing" as={Link} to="/category/women" onClick={handleItemClick}>
-        Women
-      </Dropdown.Item>
-      <Dropdown.Item className="dropdown-item-spacing" as={Link} to="/category/kids" onClick={handleItemClick}>
-        Kids
-      </Dropdown.Item> */}
-      {isLoggedIn ? (
-        <>
-          <Dropdown.Item className="dropdown-item-spacing" as={Link} to="/orderHistory" onClick={handleItemClick}>
-            Order History
-          </Dropdown.Item>
-          <Dropdown.Item className="dropdown-item-spacing" as="button" onClick={() => { Auth.logout(); handleItemClick(); }}>
-            Logout
-          </Dropdown.Item>
-        </>
-      ) : (
-        <>
-          <Dropdown.Item className="dropdown-item-spacing" as={Link} to="/signup" onClick={handleItemClick}>
-            Signup
-          </Dropdown.Item>
-          <Dropdown.Item className="dropdown-item-spacing" as={Link} to="/login" onClick={handleItemClick}>
-            Login
-          </Dropdown.Item>
-        </>
-      )}
-    </DropdownButton>
+    <> 
+      {/* Menu 23 dropdown - commented out
+      <div className="d-flex justify-content-between mt-5"> 
+        <div className=" d-flex justify-content-start">
+          <DropdownButton 
+            id="dropdown-item-button"
+            title="Menu 23"
+            show={isDropdownOpen}
+            onClick={handleToggle}
+            className="mainBtn"
+          >
+            <Dropdown.Item className="dropdown-item-spacing" as={Link} to="/" onClick={handleItemClick}>
+              Men
+            </Dropdown.Item>
+            {isLoggedIn ? (
+              <>
+                <Dropdown.Item className="dropdown-item-spacing" as={Link} to="/orderHistory" onClick={handleItemClick}>
+                  Order History
+                </Dropdown.Item>
+                <Dropdown.Item className="dropdown-item-spacing" as="button" onClick={() => { Auth.logout(); handleItemClick(); }}>
+                  Logout
+                </Dropdown.Item>
+              </>
+            ) : (
+              <>
+                <Dropdown.Item className="dropdown-item-spacing" as={Link} to="/signup" onClick={handleItemClick}>
+                  Woman
+                </Dropdown.Item>
+                <Dropdown.Item className="dropdown-item-spacing" as={Link} to="/login" onClick={handleItemClick}>
+                  Kids
+                </Dropdown.Item>
+              </>
+            )}
+          </DropdownButton> 
+        </div>   
+      </div>
+      */}
+
+      <div className="d-flex justify-content-start">
+      <DropdownButton 
+        id="dropdown-item-button"
+        
+        title="Menu"
+        show={isDropdownOpen}
+        onClick={handleToggle}
+        className="mainBtn"
+      >
+        <Dropdown.Item className="dropdown-item-spacing" as={Link} to="/" onClick={handleItemClick}>
+          Home
+        </Dropdown.Item>
+        {isLoggedIn ? (
+          <>
+            <Dropdown.Item className="dropdown-item-spacing" as={Link} to="/orderHistory" onClick={handleItemClick}>
+              Order History
+            </Dropdown.Item>
+            <Dropdown.Item className="dropdown-item-spacing" as="button" onClick={() => { Auth.logout(); handleItemClick(); }}>
+              Logout
+            </Dropdown.Item>
+          </>
+        ) : (
+          <>
+            <Dropdown.Item className="dropdown-item-spacing" as={Link} to="/signup" onClick={handleItemClick}>
+              Signup
+            </Dropdown.Item>
+            <Dropdown.Item className="dropdown-item-spacing" as={Link} to="/login" onClick={handleItemClick}>
+              Login
+            </Dropdown.Item>
+          </>
+        )}
+      </DropdownButton> </div>
+    </>
   );
 }
 
@@ -69,15 +101,15 @@ function Nav() {
   return (
     <header className="flex flex-col md:flex-row items-center justify-between px-1">
       <div className="flex justify-between items-center w-full md:w-auto">
-        <h1>
-          <Link to="/">
+        <h1 className="text-primary my-auto w-full text-center">
+          <Link to="/" className="text-decoration-none">
             <span role="img" aria-label="shopping bag">👟</span>
-            -Shop-Shop
+            Shop 23
           </Link>
         </h1>
       </div>
 
-      <nav className="mt-4 md:mt-0 flex justify-start w-full md:w-auto">
+      <nav className="mt-4 md:mt-0 flex justify-center w-full md:w-auto">
         <DropdownItemTagsExample isLoggedIn={Auth.loggedIn()} />
       </nav>
     </header>
